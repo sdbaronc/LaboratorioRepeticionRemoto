000ae80e5c30ba90a2aad2f9497fb2734644b075
checho <sergio.baron.2019@upb.edu.co>
2019-03-01 12:48:32 -0500

    mi primer commit

---
+++ Mayor.py
@@ -0,0 +1,9 @@
+a= int(input("ingrese un numero entero"))
+if a >0:
+	print ("el numero" + str(a) + "es mayor que 0")
+elif a == 0:
+	print ("el numeroes igual a 0")
+else:
+	print ("el numero" + str(a) + "es menor que 0")
+
+
